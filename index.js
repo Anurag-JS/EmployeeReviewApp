f583e12c426da9b39c9b7eb2130fcfeed4b78fd6
Anurag <anuragv8269@gmail.com>
2024-02-10 18:51:49 +0530

    Created Folder Structure

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // importing libraries
 
 const express = require('express');
+const passport = require('passport');
+const cookieParser = require('cookie-parser')
+const session=require('express-session');
+const expressLayouts =  require('express-ejs-layouts');
+const flash = require('connect-flash');
+const MongoStore = require('connect-mongo');
+
 
 const port = 8080;
 const app = express();
